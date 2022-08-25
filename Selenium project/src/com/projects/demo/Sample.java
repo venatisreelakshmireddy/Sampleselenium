c23a948579231a3ee859eb91c1a4c04f7f6242b1
venatisreelakshmireddy <sreelakshmireddy.venati@gmail.com>
2022-08-25 11:55:26 +0530

    fgg
    frff

---
+++ Selenium project/src/com/projects/demo/Sample.java
@@ -0,0 +1,11 @@
+package com.projects.demo;
+
+public class Sample 
+{
+public static void main(String[] args) 
+{
+int a=10;
+System.out.println("a");
+System.out.println("Sreelakshmi is a good ");
+}
+}
